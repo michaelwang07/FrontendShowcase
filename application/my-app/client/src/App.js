c2a940c0e6232f31782f57402c93a05f261037af
michaelwang07 <michaelwang07@yahoo.com>
2022-03-17 15:33:03 -0700

    milestone2 frontend

--- application/my-app/client/src/App.js
+++ application/my-app/client/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import CreateUser from './CreateUser';
 import Results from './Results';
+import Teampage from './Teampage';
+import BobbyR from './BobbyR';
+import MichaelW from './MichaelW';
+import MichaelA from './MichaelA';
+import MikeD from './MikeD';
+import WilliamR from './WilliamR';
 
 function App() {
   return (
@@ -10,6 +17,12 @@ function App() {
      <Routes>
        {/* <Route path="/" exact element ={<CreateUser/>} /> */}
        <Route path="/" exact element ={<Results/>} />
+       <Route path="/team" exact element ={<Teampage/>} />
+       <Route path="/team/BobbyR" exact element ={<BobbyR/>} />
+       <Route path="/team/MichaelW" exact element ={<MichaelW/>} />
+       <Route path="/team/MichaelA" exact element ={<MichaelA/>} />
+       <Route path="/team/MikeD" exact element ={<MikeD/>} />
+       <Route path="/team/WilliamR" exact element ={<WilliamR/>} />
      </Routes>
    </Router>
   );
