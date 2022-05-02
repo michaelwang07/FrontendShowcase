858e0734de3dc74a5dae08d31025eb9e62fa6bb2
wrattan <70415445+wrattan@users.noreply.github.com>
2022-05-02 14:57:51 -0700

    Added working example of Single Product Page as a Functional Component

---
+++ application/my-app/client/src/ProductFunctional.js
@@ -0,0 +1,68 @@
+/********************************************************************
+* Project: SFSU Software Engineering Project CSC648-848, Spring 2022
+* Author(s): Michael Wang 
+* Team: 06 
+*
+* File: Product.js
+*
+* Description: Product template for all our products
+*
+*
+********************************************************************/
+
+import {Link, renderMatches} from "react-router-dom";
+import './App.css';
+import React from "react";
+import Header from "./Header";
+import { createPopper } from '@popperjs/core';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import {Form} from "react-bootstrap";
+import Alert from 'react-bootstrap/Alert';
+import { useState, useEffect } from "react";
+import Footer from "./Footer";
+import { Button} from 'react-bootstrap';
+import Axios from "axios";
+
+
+function Products() {
+
+   const [curProduct, setCurrentProduct] = useState([]);
+
+
+   useEffect(() => {
+      getProductID();
+    }, []);
+
+
+   async function getProductID (){
+      const response = await Axios.get('http://localhost:3001/SingleProduct',
+      {
+          params: {
+              pid: sessionStorage.getItem("post"),
+          }
+      });
+      setCurrentProduct(response.data);
+      console.log(response.data);
+   };
+
+   return (
+      <div>
+
+<div className="grid">
+            {curProduct.map((val, key) => {
+                return <div>
+                    <h1>{val.pname}</h1>
+                    <h1>{val.pdescription}</h1>
+                    <h1>{val.pprice}</h1>
+                </div>
+            })}
+        </div>
+
+      </div>
+   );
+
+
+}
+
+export default Products;
